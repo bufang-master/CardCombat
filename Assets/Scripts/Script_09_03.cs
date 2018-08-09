993833a6380a4b85255a59bef363efd15e278bc3
hwb <1421956839@qq.com>
2018-08-09 10:50:50 +0800

    Network

---
+++ Assets/Scripts/Script_09_03.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Script_09_03 : MonoBehaviour {
+
+    public Text text;
+
+    int port = 10000;
+    string message = "";
+
+    private void Start()
+    {
+        text.text = "";
+    }
+
+    private void OnGUI()
+    {
+        switch (Network.peerType)
+        {
+            case NetworkPeerType.Disconnected:
+                StartServer();
+                break;
+            case NetworkPeerType.Server:
+                OnServer();
+                break;
+            case NetworkPeerType.Connecting:
+                break;
+        }
+    }
+
+    private void OnServer()
+    {
+        GUILayout.Label("服务器创建完毕，等待客户端连接");
+        int length = Network.connections.Length;
+        for (int i = 0; i < length; i++)
+        {
+            GUILayout.Label("连接服务器客户端ID" + i);
+            GUILayout.Label("连接服务器客户端IP" + Network.connections[i].ipAddress);
+            GUILayout.Label("连接服务器客户端端口号" + Network.connections[i].port);
+        }
+        if (GUILayout.Button("断开服务器"))
+        {
+            Network.Disconnect();
+            text.text = "";
+        }
+    }
+
+    private void StartServer()
+    {
+        if (GUILayout.Button("创建本机服务器"))
+        {
+            NetworkConnectionError error = Network.InitializeServer(10, port, false);
+            Debug.Log("连接状态：" + error);
+            GUILayout.Label("连接状态：" + error);
+        }
+    }
+
+    [RPC]
+    void RequestMessage(string message,NetworkMessageInfo info)
+    {
+        text.text += "\n发送者" + info.sender + ":" + message;
+    }
+}
---
+++ Assets/Scripts/Script_09_04.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Script_09_04 : MonoBehaviour {
+
+    string IP = "192.168.31.136";
+    int port = 10000;
+
+    public Text inputMessage;
+
+    public Text receiveMessage;
+    private void Start()
+    {
+        receiveMessage.text = "";
+    }
+    private void OnGUI()
+    {
+        switch (Network.peerType)
+        {
+            case NetworkPeerType.Disconnected:
+                StartServer();
+                break;
+            case NetworkPeerType.Server:
+                break;
+            case NetworkPeerType.Connecting:
+                break;
+            case NetworkPeerType.Client:
+                break;
+        }
+    }
+
+    private void StartServer()
+    {
+        if (GUILayout.Button("连接服务器"))
+        {
+            NetworkConnectionError error = Network.Connect(IP, port);
+            Debug.Log("连接状态：" + error);
+            GUILayout.Label("连接状态：" + error);
+        }
+    }
+
+    public void SendMessage()
+    {
+        Debug.Log("send" + inputMessage.text + Network.peerType);
+        if (Network.peerType == NetworkPeerType.Client)
+        {
+            Debug.Log("send" + inputMessage.text);
+            GetComponent<NetworkView>().RPC("RequestMessage", RPCMode.All, inputMessage.text);
+        }
+    } 
+
+    [RPC]
+    void RequestMessage(string message,NetworkMessageInfo info)
+    {
+        Debug.Log("receive" + inputMessage.text);
+        receiveMessage.text += "\n发送者" + info.sender + ":" + message;
+    }
+}
